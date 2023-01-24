d1909feed4cd6f3b423d80fa2777294d02630a8b
MarcoBardalesRodriguez <marcoantoniobardalesrodriguez@gmail.com>
2023-01-24 09:50:49 -0500

    update remote db

--- api/v1/Model/remote-config.php
+++ api/v1/Model/remote-config.php
@@ -1,7 +1,7 @@
 <?php
 //MySQL Database
-define("DB_HOST","containers-us-west-53.railway.app");
-define("DB_USERNAME","root");
-define("DB_PASSWORD","SIeFjUr4qfQFlmYyR0if");
-define("DB_NAME","railway");
-define("DB_PORT","7258");
+define("DB_HOST","localhost");
+define("DB_USERNAME","id20193339_root");
+define("DB_PASSWORD","SW\$YZ{DKE3*+Fk~!");
+define("DB_NAME","id20193339_movies");
+define("DB_PORT","3306");
